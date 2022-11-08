daff2de95077c0253f89c9cecd84cf8319254616
ahmadchand <ahmad.chand@ili.digital>
2022-11-08 18:25:10 +0500

    initial commit

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,19 @@
+# ALIAS COMMANDS
+alias ls="exa --icons --group-directories-first"
+alias ll="exa --icons --group-directories-first -l"
+alias status="exa --long --git"  
+alias cls="clear"
+
+# Directories
+alias pj="cd /mnt/d/Company/Projects"
+alias vds="cd /mnt/d/Company/Projects/VolksBank/Volksbank\ Dashboard/volksbank-digital-solutions-api-dotnet"
+alias result-analysis="cd /mnt/d/Company/Projects/VolksBank/Volksbank\ Gecco/result-analysis"
+
+# Git
+alias gstatus="git status"
+alias gaa="git add ."
+alias gcam="git commit -am"
+alias gplo="git pull origin" 
+alias gpho="git push origin"
+alias glg="git log"
+alias gcout="git checkout"
---
+++ .zshrc
@@ -0,0 +1,17 @@
+[[ -f ~/.zsh/aliases.zsh ]] && source ~/.zsh/aliases.zsh
+[[ -f ~/.zsh/functions.zsh ]] && source ~/.zsh/functions.zsh
+[[ -f ~/.zsh/starship.zsh ]] && source ~/.zsh/starship.zsh
+[[ -f ~/.zsh/nvm.zsh ]] && source ~/.zsh/nvm.zsh
+[[ -f ~/.zsh/wsl2fix.zsh ]] && source ~/.zsh/wsl2fix.zsh
+
+# Plugins
+plugins=( 
+git 
+dotnet  
+zsh-autosuggestions 
+zsh-autocomplete
+zsh-syntax-highlighting 
+)
+
+# Load Starship
+eval "$(starship init zsh)"
